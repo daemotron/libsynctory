99167dc641b3e8dafe549bd5090bd1a7cc8494a4
Daemotron <mail@daemotron.net>
2011-06-18 21:52:53 +0200

    Added some spice to AVL tree in order to render it useful for libsynctory

---
+++ src/lib/_tree.h
@@ -0,0 +1,77 @@
+/*-
+ * Copyright (c) 2011 Daemotron <mail@daemotron.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+
+#ifndef __LIBSYNCTORY_TREE_H_
+#define __LIBSYNCTORY_TREE_H_
+
+
+#include <errno.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "tree.h"
+
+
+typedef struct
+{
+    uint64_t position;
+    unsigned char *strong_checksum;
+} _tree_payload_t;
+
+
+typedef struct _tree_node_s
+{
+    uint32_t                    checksum;       /* identifying element, weak checksum   */
+    int                         payloads;       /* number of payloads in payload array  */
+    _tree_payload_t            *payload;        /* payload array                        */
+    TREE_ENTRY(_tree_node_s)    linkage;        /* magic from AVL tree implementation   */
+} _tree_node_t;
+
+
+#define _tree_node_append_payload(node, pos, checksum, size, status) {                                                  \
+    (node)->payloads++;                                                                                                 \
+    (node)->payload = (_tree_payload_t *)realloc((node)->payload, (node)->payloads * sizeof(_tree_payload_t));          \
+    if (NULL == (node)->payload) {                                                                                      \
+        free((node)->payload);                                                                                          \
+        *(status) = errno;                                                                                              \
+    }                                                                                                                   \
+    else {                                                                                                              \
+        (node)->payload[(node)->payloads - 1].position = (pos);                                                         \
+        (node)->payload[(node)->payloads - 1].strong_checksum = (unsigned char *)malloc((size));                        \
+        if (NULL == (node)->payload[(node)->payloads - 1].strong_checksum) {                                            \
+            free((node)->payload[(node)->payloads - 1].strong_checksum);                                                \
+            *(status) = errno;                                                                                          \
+        }                                                                                                               \
+        else {                                                                                                          \
+            memcpy((node)->payload[(node)->payloads - 1].strong_checksum, (checksum), (size));                          \
+            *(status) = 0;                                                                                              \
+        }                                                                                                               \
+    }                                                                                                                   \
+}
+
+
+typedef TREE_HEAD(_tree_head_s, _tree_node_s) _tree_t;
+
+TREE_DEFINE(_tree_node_s, linkage)
+
+
+int             _tree_node_compare(_tree_node_t *lhs, _tree_node_t *rhs);
+_tree_node_t   *_tree_node_new(uint32_t checksum);
+_tree_t        *_tree_new(int (*compare)(_tree_node_t *lhs, _tree_node_t *rhs));
+
+#endif /* __LIBSYNCTORY_TREE_H_ */
---
+++ src/lib/tree.c
@@ -0,0 +1,54 @@
+/*-
+ * Copyright (c) 2011 Daemotron <mail@daemotron.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+
+#include <stdlib.h>
+
+#include "_tree.h"
+
+
+int
+_tree_node_compare(_tree_node_t *lhs, _tree_node_t *rhs)
+{
+    return (lhs->checksum >= rhs->checksum) - (rhs->checksum >= lhs->checksum);
+}
+
+
+_tree_node_t *
+_tree_node_new(uint32_t checksum)
+{
+    _tree_node_t *self = (_tree_node_t *)calloc(1, sizeof(_tree_node_t));
+    if (NULL == self)
+        return NULL;
+    
+    self->checksum = checksum;
+    self->payload = NULL;
+    self->payloads = 0;
+    
+    return self;
+}
+
+
+_tree_t *
+_tree_new(int (*compare)(_tree_node_t *lhs, _tree_node_t *rhs))
+{
+    _tree_t *self = (_tree_t *)calloc(1, sizeof(_tree_t));
+    if (NULL == self)
+        return NULL;
+    
+    TREE_INIT(self, compare);
+    return self;
+}
